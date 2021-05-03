ee0b1823d9a28803883d4542320484a6fde8c10a
vangoldtoken <80657479+vangoldtoken@users.noreply.github.com>
2021-05-03 13:12:57 +0100

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -14,6 +14,7 @@ export const VGD = new Token(
   'VGD',
   'Vangold Token'
 )
+export const CAKE = new Token(ChainId.MAINNET, '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', 18, 'CAKE', 'PANCAKE TOKEN')
 export const WBNB = new Token(ChainId.MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'WBNB', 'Wrapped BNB')
 export const DAI = new Token(ChainId.MAINNET, '0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3', 18, 'DAI', 'Dai Stablecoin')
 export const BUSD = new Token(ChainId.MAINNET, '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56', 18, 'BUSD', 'Binance USD')
